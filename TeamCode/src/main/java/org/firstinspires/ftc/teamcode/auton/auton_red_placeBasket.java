ec4eee50b31c239977dacce2c0dd90d091896c53
Hitanshee <hitansheesheth08@gmail.com>
2024-10-28 17:43:06 -0400

    wrote pseudo code for auton red alliance, near basket side. placing blocks inside the highest of basket.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/auton_red_placeBasket.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.auton;
+//this is TIME BASED
+// TODO CHANGE NUMBERS AFTER
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.utilities.SimpleMecanumDrive;
+
+@Autonomous(name="redHuman", group="Linear OpMode")
+@Config
+public class auton_red_placeBasket {
+
+    //start timer
+    //move forward 1 block
+    //move left 0.2 block
+    //move forward 0.3 block
+    //pick up sample
+    //move in reverse 1 block
+    //move left 0.2 block
+    //place sample in basket
+    //move forward 1.2 block
+    //pick up sample
+    //move reverse 1.2 block
+    //place sample in basket
+    //move left 0.2 block
+    //move forward 1.2 block
+    //pick up sample
+    //move reverse 1.2 block
+    //move right 0.2 block
+    //place block in basket
+    //move right 5 block
+    //reverse 0.2 block
+    //park robot back to starting point
+
+}
