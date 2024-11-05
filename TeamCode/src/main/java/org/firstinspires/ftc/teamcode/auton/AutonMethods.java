65d06120d15e1fc332412199c4ff79db0989a7a8
Hitanshee <hitansheesheth08@gmail.com>
2024-11-05 16:53:48 -0500

    Created the methods and called it into the auton code. I extend the slides, use claw rotator, open and close the claws, move in reverse and forward

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AutonMethods.java
@@ -0,0 +1,145 @@
+package org.firstinspires.ftc.teamcode.auton;
+
+import static org.firstinspires.ftc.teamcode.utilities.CONFIG.clawRotator;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+import org.firstinspires.ftc.teamcode.utilities.Claw;
+import org.firstinspires.ftc.teamcode.utilities.Slides;
+import org.firstinspires.ftc.teamcode.utilities.SimpleMecanumDrive;
+import org.firstinspires.ftc.teamcode.utilities.ClawRotator;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+
+
+public abstract class AutonMethods extends LinearOpMode {
+    public SimpleMecanumDrive drivetrain;
+    public ClawRotator clawrotator;
+    public Claw claw;
+    public Slides slides;
+
+
+    public void runOpmode() throws InterruptedException{
+        drivetrain = new SimpleMecanumDrive(hardwareMap);
+        clawrotator = new ClawRotator(hardwareMap, telemetry);
+        claw = new Claw(hardwareMap, telemetry);
+        slides = new Slides(hardwareMap, telemetry);
+    }
+
+    public final double POWER = 0.5;
+
+public void clawOpen(){
+    claw.open();
+    sleep(5000);
+}
+
+public void slidesExtend(){
+    slides.extend();
+    sleep(6000);
+}
+
+public void slidesRetract(){
+    slides.retract();
+}
+
+public void clawClose(){
+    claw.close();
+}
+    public void stopDrive(){
+        drivetrain.move(0,0,0);
+        sleep(100);
+    }
+
+    // Going forward, backward, turning, going left, going right
+    public void backOneee() {
+
+       //goes in reverse a little more than 1, about 1.2
+        drivetrain.move(0, POWER, 0);
+        sleep(1450);
+        stopDrive();
+    }
+
+    public void rightOneHalf() {
+        drivetrain.move(-POWER, 0, 0);
+        sleep(3000);
+        stopDrive();
+    }
+
+    public void forwardOneeee(){
+        drivetrain.move(0, -POWER, 0);
+        sleep(1450);
+        stopDrive();
+    }
+
+    public void clawDrop(){
+        clawrotator.toDrop();
+        sleep(1450);
+    }
+
+    public void clawPick(){
+    clawrotator.toPick();
+    }
+
+
+    public void rightFive() {
+        // goes to the ending spot in auton
+        drivetrain.move(-POWER,0, 0);
+        sleep(5200);
+        stopDrive();
+    }
+
+    public void leftOneHalf()  {
+        //should be a little less than 0.5 around 0.2
+        drivetrain.move(POWER, 0, 0);
+        sleep(3000);
+        stopDrive();
+    }
+    public void forwardHalf() {
+        drivetrain.move(0,-POWER, 0);
+        sleep(900);
+        stopDrive();
+    }
+
+    public void backOneHalf() {
+        drivetrain.move(0,POWER, 0);
+        sleep(450);
+        stopDrive();
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+    //move forward 1 block
+
+    //move left 0.2 block
+    //move forward 0.3 block
+    //pick up sample
+    //move in reverse 1 block
+    //move left 0.2 block
+    //place sample in basket
+    //move forward 1.2 block
+    //pick up sample
+    //move reverse 1.2 block
+    //place sample in basket
+    //move left 0.2 block
+    //move forward 1.2 block
+    //pick up sample
+    //move reverse 1.2 block
+    //move right 0.2 block
+    //place block in basket
+    //move right 5 block
+    //reverse 0.2 block
+    //park robot back to starting point
+
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/auton_red_nearBasket.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/auton_red_nearBasket.java
@@ -6,41 +6,90 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.auton.AutonMethods;
 import org.firstinspires.ftc.teamcode.utilities.SimpleMecanumDrive;
+
+
 
 @Autonomous(name="redHuman", group="Linear OpMode")
 @Config
-public class auton_red_nearBasket extends LinearOpMode{
+public class auton_red_nearBasket extends AutonMethods {
     public final double POWER = 0.5;
+
     public void runOpMode() throws InterruptedException {
-        telemetry.addLine("STARTING! (hopfully)");
+        telemetry.addData("Initialized:", "Hopefully");
         telemetry.update();
-    //start timer
-    ElapsedTime runtime = new ElapsedTime();
-    SimpleMecanumDrive drivetrain = new SimpleMecanumDrive(hardwareMap);
+        //start timer
+        ElapsedTime runtime = new ElapsedTime();
+        SimpleMecanumDrive drivetrain = new SimpleMecanumDrive(hardwareMap);
 
-    waitForStart();
-    runtime.reset();
-    //move forward 1 block
-    //move left 0.2 block
-    //move forward 0.3 block
-    //pick up sample
-    //move in reverse 1 block
-    //move left 0.2 block
-    //place sample in basket
-    //move forward 1.2 block
-    //pick up sample
-    //move reverse 1.2 block
-    //place sample in basket
-    //move left 0.2 block
-    //move forward 1.2 block
-    //pick up sample
-    //move reverse 1.2 block
-    //move right 0.2 block
-    //place block in basket
-    //move right 5 block
-    //reverse 0.2 block
-    //park robot back to starting point
 
+        waitForStart();
+        runtime.reset();
+
+        //move forward 1 block
+        forwardOneeee();
+        stopDrive();
+        //move left 0.2 block
+        leftOneHalf();
+        //move forward 0.3 block
+        forwardHalf();
+        //pick up sample
+        clawOpen();
+        clawClose();
+        //move in reverse 1 block
+        backOneee();
+        //move left 0.2 block
+        leftOneHalf();
+        //place sample in basket
+        slidesExtend();
+        clawDrop();
+        clawOpen();
+        clawClose();
+        clawPick();
+        slidesRetract();
+        //move forward 1.2 block
+        forwardOneeee();
+        //pick up sample
+        clawOpen();
+        clawClose();
+        //move reverse 1.2 block
+        backOneee();
+        //place sample in basket
+        slidesExtend();
+        clawDrop();
+        clawOpen();
+        clawClose();
+        clawPick();
+        slidesRetract();
+        //move left 0.2 block
+        leftOneHalf();
+        //move forward 1.2 block
+        forwardOneeee();
+        //pick up sample
+        clawOpen();
+        clawClose();
+        //move reverse 1.2 block
+        backOneee();
+        //move right 0.2 block
+        rightOneHalf();
+        //place block in basket
+        slidesExtend();
+        clawDrop();
+        clawOpen();
+        clawClose();
+        clawPick();
+        slidesRetract();
+        //move right 5 block
+        rightFive();
+        //reverse 0.2 block
+        backOneHalf();
+        //park robot back to starting point
+        stopDrive();
+
+
+
+        telemetry.addLine("Run time:" +  runtime.toString());
+        telemetry.update();
     }
 }
