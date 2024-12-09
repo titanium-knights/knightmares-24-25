54b11771d06bbf1e007135fe8499007cf747a449
SalC124 <saltcal124@gmail.com>
2024-12-09 17:50:10 -0500

    i understand it now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/Claw.java
@@ -1,19 +1,19 @@
 package org.firstinspires.ftc.teamcode.utilities;
 
-import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
+import androidx.annotation.NonNull;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
 
 //:)
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 
-    @Config
+@Config
     public class Claw {
 
         Servo clawServo;
@@ -36,12 +36,35 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
             telemetry.addLine("open claw");
             clawServo.setPosition(openPos);
         }
+
+        public class OpenClaw implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                open();
+                return false;
+            }
+        }
+
+        public Action openAction() {  return new OpenClaw();  }
+
+
+
         public void close() {
             telemetry.addLine("close claw");
             clawServo.setPosition(closedPos);
         }
 
+        public class CloseClaw implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                close();
+                return false;
+            }
+        }
+
+        public Action closeAction() {  return new CloseClaw();  }
+
         public double getPosition() {
             return (clawServo.getPosition());
         }
-    }
+}
