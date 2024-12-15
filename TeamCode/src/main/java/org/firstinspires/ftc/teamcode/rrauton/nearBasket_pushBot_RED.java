7d673bbca85c0c2586c9a3916841183fb6b86549
IpekOruc <ipekoruc23@gmail.com>
2024-12-15 09:42:22 -0500

    finished push bot for park and basket FOR RED, the x and y numbers are approximate THE TANGENT RADIANS ARE RANDOM , NOT TUNED

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rrauton/nearBasket_pushBot_RED.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rrauton/nearBasket_pushBot_RED.java
@@ -16,7 +16,7 @@ import org.firstinspires.ftc.teamcode.utilities.Slides;
 
 @Config
 @Autonomous(name = "nearPark_pushBot", group = "Autonomous")
-public class nearBasket_pushBot extends LinearOpMode {
+public class nearBasket_pushBot_RED extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -42,25 +42,28 @@ public class nearBasket_pushBot extends LinearOpMode {
         //TODO ALL NUMBERS ARE NOT TUNED
         // (the radians 100%) BUT the x and y's are approximate assume robot in center of block
         TrajectoryActionBuilder tab = drivetrain.actionBuilder(begPose)
-                //todo start from y = 60 x = 12
+                //todo start from y = 60 x = -12
                 .lineToX(-36) // up 1 block,
                 .setTangent(Math.toRadians(90)) // might be 270, needs to be tuned
-                .lineToY(-12) // 3 blocks left
+                .lineToY(12) // 3 blocks left
                 .setTangent(Math.toRadians(0))
-                .lineToX(-44) // move up by 1/3 of a block
+                .lineToX(-44) // move up by 1/3 of a block (8 inches?)
                 .setTangent(Math.toRadians(90))
-                .lineToY(-60) //move right into park
-                .lineToY(-12) //move left to the samples
+                .lineToY(60) //move right into park
+                .lineToY(12) //move left to the samples
                 .setTangent(Math.toRadians(0))
                 .lineToX(-52) //move up to middle sample
                 .setTangent(Math.toRadians(90))
-                .lineToY(-60) //move right into park
-                .lineToY(-12) //move left to the samples
+                .lineToY(60) //move right into park
+                .lineToY(12) //move left to the samples
                 .setTangent(Math.toRadians(0))
                 .lineToX(-60) //move up to highest sample
                 .setTangent(Math.toRadians(90))
-                .lineToY(-60) //move right into park
-                //FINAL PARK
+                .lineToY(60) //move right into park
+                //currently should be in basket
+
+                .setTangent(Math.toRadians(0))
+                .lineToX(36) //move up towards the small portion of park
         ;
 
 
