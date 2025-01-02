4085634ed31b48dfbb36b2a9486b3339e7d09c71
lejubi <113481201+lejubi@users.noreply.github.com>
2025-01-02 17:12:09 -0500

    pedro

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/LConstants.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.pedroPathing.constants;
+
+import com.pedropathing.localization.*;
+import com.pedropathing.localization.constants.*;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+public class LConstants {
+    static {
+        PinpointConstants.forwardY = 50.8;
+        PinpointConstants.strafeX = -81.28;
+        PinpointConstants.distanceUnit = DistanceUnit.MM;
+        // TODO: make sure this is the same name as the I2C port (should not be port 0)
+        PinpointConstants.hardwareMapName = "pinpoint";
+        PinpointConstants.useYawScalar = false;
+        PinpointConstants.yawScalar = 1.0;
+        PinpointConstants.useCustomEncoderResolution = false;
+        PinpointConstants.encoderResolution = GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD;
+        PinpointConstants.customEncoderResolution = 13.26291192;
+        PinpointConstants.forwardEncoderDirection = GoBildaPinpointDriver.EncoderDirection.REVERSED;
+        PinpointConstants.strafeEncoderDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
+
+    }
+}
+
+
+
+
